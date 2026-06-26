04fd97449bf71cb6bfcf871970e4fa3eaa4d2ce6
cvsz <seaza@msn.com>
2026-06-27 01:39:42 +0700

    fix: expose portable uname fallback

--- zai_coder/__init__.py
+++ zai_coder/__init__.py
@@ -8,14 +8,15 @@ __all__ = ["__version__"]
 __version__ = "0.1.3"
 
 
-if not hasattr(_os, "uname"):
-    def _portable_uname():
-        return _SimpleNamespace(
-            sysname=_platform.system(),
-            nodename=_platform.node(),
-            release=_platform.release(),
-            version=_platform.version(),
-            machine=_platform.machine(),
-        )
+def _portable_uname():
+    return _SimpleNamespace(
+        sysname=_platform.system(),
+        nodename=_platform.node(),
+        release=_platform.release(),
+        version=_platform.version(),
+        machine=_platform.machine(),
+    )
 
+
+if not hasattr(_os, "uname"):
     _os.uname = _portable_uname
